4583150e6c314875d8eb9646027cb951b8a30bfe
DNars25 <ianars.creative@gmail.com>
2026-02-18 15:47:04 -0400

    feat: páginas de filmes e séries com descoberta TMDB

--- next.config.js
+++ next.config.js
@@ -7,7 +7,19 @@ const nextConfig = {
         hostname: 'image.tmdb.org',
       },
     ],
+    unoptimized: true,
   },
 }
 
 module.exports = nextConfig
+```
+
+Salve **CTRL + S** e feche. Depois:
+```
+git add next.config.js
+```
+```
+git commit -m "liberar imagens TMDB sem otimizacao"
+```
+```
+git push
